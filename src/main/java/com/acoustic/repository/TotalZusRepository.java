6fe8ce1ec8b8d92e56d2180dd1a477e25e79ab3c
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-05 13:07:46 +0200

    modified the gradle file and changed the id to Integer

--- src/main/java/com/acoustic/repository/TotalZusRepository.java
+++ src/main/java/com/acoustic/repository/TotalZusRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TotalZusRepository extends CrudRepository<TotalZus, Long> {
+public interface TotalZusRepository extends CrudRepository<TotalZus, Integer> {
 
 
 
